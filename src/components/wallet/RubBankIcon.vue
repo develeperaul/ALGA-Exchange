c3a2c8fadac556dbc10bd2298d20837405bbeb10
Ralfik <52822804+develeperaul@users.noreply.github.com>
2026-09-17 20:00:23 +0500

    refactor: render all bank logos from catalog

--- src/components/wallet/RubBankIcon.vue
+++ src/components/wallet/RubBankIcon.vue
@@ -1,16 +1,12 @@
 <template>
   <span class="rub-bank-icon" :class="bank.iconClass" aria-hidden="true">
-    <template v-if="bank.id === 'sovcombank'">
-      <img class="rub-bank-icon__sov rub-bank-icon__sov--a" :src="sovcombankIconParts[0]" alt="">
-      <img class="rub-bank-icon__sov rub-bank-icon__sov--b" :src="sovcombankIconParts[1]" alt="">
-    </template>
-    <img v-else-if="bank.icon" :src="bank.icon" alt="">
-    <span v-else class="rub-bank-icon__text">{{ bank.iconText }}</span>
+    <img v-if="bank.icon" :src="bank.icon" alt="">
+    <span v-else class="rub-bank-icon__text">{{ bank.label.charAt(0) }}</span>
   </span>
 </template>
 
 <script setup lang="ts">
-import { sovcombankIconParts, type RubBank } from 'pages/wallet/rubDepositData';
+import type { RubBank } from 'pages/wallet/rubDepositData';
 
 defineProps<{
   bank: RubBank;
@@ -34,27 +30,16 @@ defineOptions({
 }
 
 .rub-bank-icon img {
-  max-width: 100%;
-  max-height: 100%;
+  width: 24px;
+  height: 24px;
   display: block;
   object-fit: contain;
 }
 
-.rub-bank-icon--sber img {
-  width: 20px;
-  height: 20px;
-}
-
-.rub-bank-icon--tbank img,
-.rub-bank-icon--vtb img {
-  width: 24px;
-  height: 24px;
-}
-
+.rub-bank-icon--sber img,
 .rub-bank-icon--mts img {
   width: 20px;
   height: 20px;
-  border-radius: 8px;
 }
 
 .rub-bank-icon--sbp {
@@ -66,85 +51,10 @@ defineOptions({
   height: 20px;
 }
 
-.rub-bank-icon--sovcombank {
-  background: #F4F6F8;
-}
-
 .rub-bank-icon__text {
   color: currentColor;
   font-size: 11px;
   line-height: 1;
   font-weight: 700;
-}
-
-.rub-bank-icon--alfa {
-  background: var(--ui-surface-white);
-  color: #EF3124;
-}
-
-.rub-bank-icon--rencredit {
-  background: var(--ui-surface-white);
-  color: #E63C8A;
-}
-
-.rub-bank-icon--otp {
-  background: #C8F344;
-  color: #173B13;
-}
-
-.rub-bank-icon--ozon {
-  background: #005BFF;
-  color: var(--ui-text-inverse);
-}
-
-.rub-bank-icon--raiffeisen {
-  background: #FFE500;
-  color: #111111;
-}
-
-.rub-bank-icon--ubrir {
-  background: var(--ui-surface-white);
-  color: #E91E63;
-}
-
-.rub-bank-icon--uralsib {
-  background: var(--ui-surface-white);
-  color: #3E176E;
-}
-
-.rub-bank-icon--yandex {
-  background: #111111;
-  color: var(--ui-text-inverse);
-}
-
-.rub-bank-icon--mkb {
-  background: #D93C32;
-  color: var(--ui-text-inverse);
-}
-
-.rub-bank-icon--kazan {
-  background: var(--ui-surface-white);
-  color: #7AC143;
-}
-
-.rub-bank-icon--inbank {
-  background: var(--ui-surface-white);
-  color: #1D8B75;
-}
-
-.rub-bank-icon__sov {
-  position: absolute;
-}
-
-.rub-bank-icon__sov--a {
-  width: 16px;
-  height: 22px;
-  right: 2px;
-}
-
-.rub-bank-icon__sov--b {
-  width: 12px;
-  height: 24px;
-  left: 2px;
 }
 </style>
